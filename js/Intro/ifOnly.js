0df69fc6338e369d9f7a75df7fe866ddea03b20d
Faith Mathenge <nyamburafay04@gmail.com>
2026-06-24 10:48:32 +0300

    More If conditions

---
+++ js/Intro/ifOnly.js
@@ -0,0 +1,29 @@
+//lets start with if
+//If is special
+
+let age=prompt("Enter your age")
+alert(`you entered ${age}`)
+
+if(age>10){
+alert("you are a baby")
+}
+
+if(age>13){
+ alert("you are a teen")
+}
+
+if(age>18){
+  alert("you are an aldult")
+}
+
+if(age>27){
+  alert("Best for marriage")
+}
+
+if (age>45){
+  alert("Best age for Retirement") 
+}
+
+if (age>50){
+  alert("you are an Ancestor")
+}
