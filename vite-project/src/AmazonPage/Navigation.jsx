0140e63801c5befc88c381767205391e98952994
Faith Mathenge <nyamburafay04@gmail.com>
2026-07-28 15:18:23 +0300

    navigation section

---
+++ vite-project/src/AmazonPage/Navigation.jsx
@@ -0,0 +1,11 @@
+import logo from "./Assets/amazonlogo.png";
+
+function Navigate() {
+  return(
+    <div>
+      <h2>Navigation section</h2>
+    </div>
+  )
+}
+
+export default Navigate;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,45 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-function MyComponent() {
-  return null; //valid jsx
-}
-
-function MyComponent2() {
-  return (
-    <div>
-      <h1>Winter is coming</h1>
-      <div></div>
-    </div>
-  );
-}
-
-function MyComponent3() {
-  return (
-    <>
-      <h1>You know nothing Jon snow</h1>
-      <p>A lannister always pays his debts</p>
-    </>
-  );
-}
-
-function ParentComponent() {
-  return (
-    <div>
-      <h1>This is the parent component</h1>
-      <MyComponent />
-      <MyComponent2></MyComponent2>
-      <MyComponent3 />
-    </div>
-  );
-}
+import Navigation from "./AmazonPage/Navigation";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <h1>My first react app</h1>
-    <MyComponent />
-    <MyComponent2></MyComponent2>
-    <MyComponent3 />
-    <ParentComponent />
+    <Navigation />
   </StrictMode>,
 );
